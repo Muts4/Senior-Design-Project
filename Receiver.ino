affc2563f92ae536692131397c0dd2341644e8c5
N-Alvarez <z1909360@students.niu.edu>
2022-05-09 15:22:41 -0500

    Update
    OLED code added

--- Receiver.ino
+++ Receiver.ino
@@ -6,6 +6,18 @@
 #include "SD.h"
 #include <SPI.h>
 
+//OLED Libraries
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//OLED -- this is the Width and Height of Display which is 128 X 32 pixels
+#define OLED_WIDTH  128
+#define OLED_HEIGHT 32
+//OLED -- More OLED stuff
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
  String dataMessage_1;
  String dataMessage_2;
  String dataMessage_3;
@@ -99,6 +111,50 @@ void appendFile(fs::FS &fs, const char * path, const char * message) {
 }
 
 void setup() {
+
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize OLED with the I2C addr 0x3C (for the 128x32)
+ display.clearDisplay(); // cleans the screen
+ display.drawRect(0, 0, 127,32, WHITE); //draws a margin around the screen
+ display.setTextSize(2);
+ display.setTextColor(WHITE);
+ display.setCursor(22,10);
+ display.println("Welcome");
+
+ display.display();
+ delay(5000);
+ 
+ display.clearDisplay();
+ display.drawRect(0, 0, 127,32, WHITE); //draws a margin around the screen
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(40,3); //x,y position of the text
+ display.println("Team 41");
+ display.setCursor(10,12); //x,y position of the text
+ display.println("Grip Force Monitor");
+ display.setCursor(40,21); //x,y position of the text
+ display.println("System");
+
+ display.display();
+ delay(5000);
+ 
+ //Display main menu
+ display.clearDisplay();
+ display.drawRect(100, 2, 24, 10, WHITE); //Draw battery icon on the top right corner
+ display.fillRect(102, 5, 6, 4, WHITE); //Full battery or less
+ display.fillRect(109, 5, 6, 4, WHITE); //66.6 % of battery or less
+ display.fillRect(116, 5, 6, 4, WHITE); //33.3 % of battery or less
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(10,10);
+ display.println("Recording data");
+ display.setCursor(10,18);
+ display.println("Starts in...");
+ 
+ display.drawRect(0, 0, 127,32, WHITE);
+  
+ display.display();
+ delay(1000);
+
   // Set up Serial Monitor
   Serial.begin(115200);
   initSDCard();
@@ -131,6 +187,37 @@ void setup() {
 }
 
 void loop() {
+
+ //OLED display code
+ //define string variables for printing on OLED
+ String forceData1 =  String(myData.a, 2);
+ String forceData2 =  String(myData.b, 2);
+ String forceData3 =  String(myData.c, 2);
+ display.clearDisplay(); 
+ 
+ // Prints the sensor's data on the OLED
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(3,3); //x,y position of the text
+ display.println("Palm Sensor 1:");
+ display.setCursor(90,3); //x,y position of the incoming data
+ display.println(forceData1);
+ 
+ display.setCursor(3,12); //x,y position of the text
+ display.println("Palm Sensor 2:");
+ display.setCursor(90,12); //x,y position of the incoming data
+ display.println(forceData2);
+ 
+ display.setCursor(3,21); //x,y position of the text
+ display.println("Finger Sensor:");
+ display.setCursor(90,21); //x,y position of the incoming data
+ display.println(forceData3);
+ display.drawRect(0, 0, 127,32, WHITE); //draws a margin around the screen
+
+ display.display();
+
+//SD stuff starts
+  
    dataMessage_1 = float(myData.a);
     Serial.print("Sensor 1 data: ");
     Serial.println(dataMessage_1);
